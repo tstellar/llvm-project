f6b214fe57864123a9ccedefbe3182d9004bb23e
Konrad Kleine <kkleine@redhat.com>
2021-09-23 23:53:26 +0200

    Determine latest stable commit

---
+++ .github/workflows/get-good-commit.py
@@ -0,0 +1,67 @@
+#!/bin/env python3
+
+import sys
+from github import Github
+import argparse
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Find the latest commit that passed tests')
+    parser.add_argument('--token',
+                        dest='github_token',
+                        type=str,
+                        default="YOUR-TOKEN-HERE",
+                        help="your github token")
+    parser.add_argument('--project',
+                        dest='github_project',
+                        type=str,
+                        default="llvm/llvm-project",
+                        help="github project to use (default: llvm/llvm-project")
+    parser.add_argument('--start-ref',
+                        dest='start_ref',
+                        type=str,
+                        default="bbc74a11bc15f4ce962ac7f915309145dcfc2582",
+                        help="git reference (e.g. branch name or sha1) to check first (default: main)")
+    parser.add_argument('--max-tries',
+                        dest='max_tries',
+                        type=int,
+                        default="20",
+                        help="how many commit to try before giving up (default: 10)")
+    parser.add_argument('--ensure-checks',
+                        dest='ensure_checks',
+                        metavar='CHECK',
+                        nargs='+',
+                        default=["clang-x86_64-debian-fast", "llvm-clang-x86_64-expensive-checks-debian"],
+                        type=str,
+                        help="list check names that must have run (default: clang-x86_64-debian-fast, llvm-clang-x86_64-expensive-checks-debian)")
+
+    args = parser.parse_args()
+    
+    g = Github(login_or_token=args.github_token)
+    repo = g.get_repo(args.github_project)
+    sha=args.start_ref
+
+    for i in range(0, args.max_tries):
+        commit = repo.get_commit(sha=sha)
+        combined_status = commit.get_combined_status().state
+        if combined_status != "success":
+            # move on with parent if combined status is not successful
+            sha=commit.parents[0].sha
+            continue
+        
+        ok = False
+        statuses = commit.get_statuses()
+        checks = args.ensure_checks
+        for status in statuses:
+            if status.context in checks:
+                checks.remove(status.context)
+        if len(checks) != 0:
+            # not all checks were found, continue with parent commit
+            sha=commit.parents[0].sha
+            continue
+        
+        print(commit.sha)
+        sys.exit(0)
+    sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
