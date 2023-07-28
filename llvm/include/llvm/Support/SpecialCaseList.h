6bbeebaaf8c17e0f45cf97afa0842ecac0de18bd
Tom Stellard <tstellar@redhat.com>
2023-07-28 02:46:12 +0000

    XXX

--- llvm/include/llvm/Support/SpecialCaseList.h
+++ llvm/include/llvm/Support/SpecialCaseList.h
@@ -124,9 +124,7 @@ protected:
     bool insert(std::string Regexp, unsigned LineNumber, std::string &REError);
     // Returns the line number in the source file that this query matches to.
     // Returns zero if no match is found.
-    Matcher() = default;
     unsigned match(StringRef Query) const;
-    Matcher(Matcher const &) = delete;
     Matcher &operator=(Matcher const &) = delete;
 
   private:
