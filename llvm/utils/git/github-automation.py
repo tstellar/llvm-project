fadc4b57852f5a9906a8d9fe14b6b08506b5ba8f
Tom Stellard <tstellar@redhat.com>
2024-05-30 13:45:39 -0700

    Patch to trigger format error

--- llvm/utils/git/github-automation.py
+++ llvm/utils/git/github-automation.py
@@ -96,7 +96,7 @@ Author: {self.issue.user.name} ({self.issue.user.login})
 
 
 def human_readable_size(size, decimal_places=2):
-    for unit in ["B", "KiB", "MiB", "GiB", "TiB", "PiB"]:
+    for unit in ['B', "KiB", "MiB", "GiB", "TiB", "PiB"]:
         if size < 1024.0 or unit == "PiB":
             break
         size /= 1024.0
